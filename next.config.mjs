22e78c33691bdbef37cbf20f082aeb0c579b7009
IAthen-wq <I.Athen@yandex.com>
2026-02-06 20:01:50 +0300

    Правки

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    webpackMemoryOptimizations: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
